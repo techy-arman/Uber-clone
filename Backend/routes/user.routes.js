78bcc1c05da5860f830538b588e908820bd64b9a
Arman Ansari <you@example.com>
2025-01-29 00:26:08 +0530

    user login done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,22 +1,36 @@
 const express = require("express")
 const router = express.Router()
-const {body} = require("express-validator")
+const { body } = require("express-validator")
 const userController = require("../controllers/user.controller")
 
-try{
-    router.post('/register',[
+try {
+    router.post('/register', [
         body("email").isEmail().withMessage("Please enter a valid email."),
-        body("fullname.firstname").isLength({min:3}).withMessage("Firsname must contain atleast 3 characters."),
-        body("password").isLength({min:5}).withMessage("Password must contain atleast 5 characters.")
-    
+        body("fullname.firstname").isLength({ min: 3 }).withMessage("Firsname must contain atleast 3 characters."),
+        body("password").isLength({ min: 5 }).withMessage("Password must contain atleast 5 characters.")
+
     ],
-    
-    userController.registerUser
-    
+
+        userController.registerUser
+
     )
 }
-catch(err){
-    console.log("Post error",err)
+catch (err) {
+    console.log("Post error", err)
+}
+try {
+    router.post('/login', [
+        body("email").isEmail().withMessage("Please enter a valid email."),
+        body("password").isLength({ min: 5 }).withMessage("Password must contain atleast 5 characters.")
+
+    ],
+
+        userController.loginUser
+
+    )
+}
+catch (err) {
+    console.log("Post error", err)
 }
 
-module.exports=router;
+module.exports = router;
